5a36734fffc1728fbb68428a61b9a4b75ed7c742
jampol <rafaellucenamartinez@mbp-de-rafael.home>
2017-01-14 13:37:30 +0100

    I´m doing tests in the file test.js
    All that I´d already tested is commented with /* */

--- test.js
+++ test.js
@@ -3,7 +3,82 @@
  */
 
 
+/*
+
+function myFunction(){
+
+    document.write(this.name);
+    document.write("<br>");
+}
+
+var objectA = {
+
+    name: 'Alice',
+    myMethod: myFunction
+};
 
 
+var objectB = {
+
+    name:'Bob',
+    myMethod: myFunction
+};
 
 
+objectA.myMethod();
+objectB.myMethod();
+
+*/
+
+/*
+
+function esperarUnSegundo(callback){
+    setTimeout(function(){
+        callback();
+    }, 1000);
+}
+
+
+var alice = {
+
+    nombre: 'Alice',
+    cansarse: function(){
+        alert(this.nombre + 'se ha cansado de esperar');
+    }
+
+};
+
+esperarUnSegundo(alice.cansarse);*/
+
+/*
+
+var alice = {
+
+    nombre: 'Alice',
+    cansarse: function(){
+        console.log(this,nombre);
+    }
+};
+
+
+var myFunction = alice.cansarse();
+
+*/
+
+
+var prueba = {
+
+    nombre : 'Antonia'
+}
+
+var alice = {
+
+    nombre : 'Alice',
+    saludar: function(amigo1,amigo2){
+        alert('Hola'+amigo1+'Hola'+amigo2+', yo soy'+this.nombre);
+
+    }
+}
+
+var myFunction = alice.saludar;
+myFunction.call(prueba,'juan','pedro');
